633d4ed45004b88e1683283f66c2fc961faacf5b
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-25 14:46:44 +0600

    create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () =>{
+    return(
+        <>
+        <div className='max-w-6xl mx-auto space-y-4'>
+            <h1 className='text-2xl font-medium text-yellow-600'>About</h1>
+           <p> IMDb (an acronym for Internet Movie Database) is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews. IMDb began as a fan-operated movie database on the Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993. Since 1998, it has been owned and operated by IMDb.com, Inc., a subsidiary of Amazon.</p>
+           <p>The site's message boards were disabled in February 2017. As of 2019, IMDb was the 52nd most visited website on the Internet, as ranked by Alexa. As of March 2022, the database contained some 10.1 million titles (including television episodes), 11.5 million person records, and 83 million registered users.</p>
+           <p>Actors, crew, and industry executives can post their own resume and upload photos of themselves for a yearly membership fee to IMDbPro. IMDbPro can be accessed by anyone willing to pay the annual fee of US$149.99. Membership enables a user to access the rank order of each industry personality, as well as agent contact information for any actor, producer, director etc. that has an IMDb page. IMDbPro also allows existing actors to claim their name page, as well as production companies to claim titles they own/manage. Enrolling in IMDbPro enables members who are industry personnel to upload a head shot to open their page, as well as to upload hundreds of photos to accompany their page. Anyone can register as an IMDb user and contribute to the site as well as view its content; however, those users enrolled in IMDbPro have greater access and privileges</p>
+        </div>
+        </>
+    )
+}
+
+export default About;
