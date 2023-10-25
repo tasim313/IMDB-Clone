e7133b26f0cc3e1930230b66921ee3f824ed082b
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-25 11:30:55 +0600

    create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import './globals.css'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body 
+      className={inter.className}>
+        {/* Header */}
+        <Header></Header>
+
+        {/*Navbar */}
+
+         {/*SearchBox */}
+        {children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <div>
-    <h1>This is IMDB Clone</h1>
-    <h2 className='text-red-400'>Home</h2>
+    
    </div>
   )
 }
