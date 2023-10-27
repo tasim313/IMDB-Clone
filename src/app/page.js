9533232a0d418b540d516888069d0dfa90c0f3c8
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-27 21:58:39 +0600

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
+
+import Results from '@/components/Result'
 import Image from 'next/image'
 
+const API_KEY = process.env.API_KEY
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); 
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+ 
+
   return (
    <div>
-    
+       <Results results={results}/>
    </div>
   )
 }
