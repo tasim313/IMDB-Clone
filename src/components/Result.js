fa7b3239df5ffa8525f33e543d103f822706da85
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-28 00:34:44 +0600

    add card component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Card from "./Card";
+
 
 const Results = ({ results }) =>{
     return(
         <>
         <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
                 {results.map((result) => (
-                    <div key={result.id}>{result.original_title}</div>
-                    // <Card key={result.id} result={result} />
+                
+                    <Card key={result.id} result={result} />
+                    
                 ))}
         </div>
         </>
