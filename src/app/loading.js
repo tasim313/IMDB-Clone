24be4cd86a40254b148ac5b230ba4d1103235e65
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-01 11:32:58 +0600

    update loading and about page

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const Loading = () =>{
     return(
         <>
         <div className="flex justify-center">
-          <img className="h-96" src="spinner.svg" alt="loading..."/>
+          {/* <img className="h-96" src="spinner.svg" alt="loading..."/> */}
+          <Image width={500} height={300} className="h-96" src="spinner.svg" alt="loading..."/>
         </div>
         </>
     )
